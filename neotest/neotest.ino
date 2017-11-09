ecf575e6607ba54efb100cbaad570c74d12a8014
Barry Bridgman <barry@bridge-digital.co.uk>
2017-11-09 09:27:02 +0000

    Updated rpm bar.

--- neotest/neotest.ino
+++ neotest/neotest.ino
@@ -12,20 +12,29 @@
 #include <Adafruit_NeoPixel.h>
  
 #define PIN      6
-#define N_LEDS 8
+#define N_LEDS 16
  
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
  
 void setup() {
   strip.begin();
+  strip.setBrightness(50);
 }
 
-uint32_t colours[8] = { strip.Color(0,255,0),
+uint32_t colours[16] = { strip.Color(0,255,0),
+                        strip.Color(0,255,0),
+                        strip.Color(0,255,0),
+                        strip.Color(0,255,0),
+                        strip.Color(0,255,0),
                         strip.Color(0,255,0),
                         strip.Color(0,255,0),
                         strip.Color(0,255,0),
                         strip.Color(255,255,0),
                         strip.Color(255,255,0),
+                        strip.Color(255,255,0),
+                        strip.Color(255,255,0),
+                        strip.Color(255,0,0),
+                        strip.Color(255,0,0),
                         strip.Color(255,0,0),
                         strip.Color(255,0,0)};
                         
@@ -37,6 +46,15 @@ void loop() {
   //chase(strip.Color(255, 255, 255)); // White
   bar();
 }
+
+static all_same_colour(uint32_t colour) {
+   for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i , colour); // Draw new pixel
+   }
+    
+    strip.show();
+
+}
  
 static void chase(uint32_t c) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
@@ -45,6 +63,14 @@ static void chase(uint32_t c) {
       delay(25);
   }
 
+  for(uint16_t i=0; i < 5; i++) {
+    all_same_colour(strip.Color(255,255,255));
+    delay(10);
+    all_same_colour(0);
+    delay(10);
+
+  }
+  
   for(uint16_t i=0; i<strip.numPixels(); i++) {
       strip.setPixelColor(strip.numPixels()-i , 0); // Draw new pixel
       strip.show();
@@ -52,20 +78,38 @@ static void chase(uint32_t c) {
   }
 
   
+}
+
+static set_bar(uint8_t value) {
+  all_same_colour(0);
+
+  for(uint8_t i=0; i< value; i++) {
+    strip.setPixelColor(i  , colours[i]); // Draw new pixel
+  }
+
+  strip.show();
 }
 
 static void bar(void) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i  , colours[i]); // Draw new pixel
-      strip.show();
+
+  for(uint16_t i=0; i<=strip.numPixels(); i++) {
+      set_bar(i);
       delay(25);
   }
 
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(strip.numPixels()-i , 0); // Draw new pixel
-      strip.show();
-      delay(25);
+  delay(100);
+
+  for(uint16_t i=0; i < 10; i++) {
+    all_same_colour(strip.Color(255,255,255));
+    delay(20);
+    all_same_colour(0);
+    delay(20);
   }
 
-  
+
+  for(uint16_t i=0; i<= strip.numPixels(); i++) {
+      set_bar(strip.numPixels() -i);
+      delay(25);
+  }
 }
+
